94e29ed447a1958bacfd643095961e2cfcb8cff9
lucask84ever <fox_krb@yahoo.com.br>
2021-10-24 11:13:27 -0300

    Storyboard

---
+++ KantoPokedex/View/PokemonDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailViewController.swift
+//  KantoPokedex
+//
+//  Created by lucas.silva on 24/10/21.
+//
+
+import UIKit
+
+final class PokemonDetailViewController: UIViewController {
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("APARECI")
+    }
+}
